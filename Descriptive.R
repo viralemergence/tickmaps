244045b772831b916efe8716f2932ca6bda69e40
Colin J. Carlson <cjcarlson@berkeley.edu>
2020-12-09 21:12:57 -0500

    halfway point, minus maps
    Needs a revision of two pies and addition of two more pies

---
+++ Descriptive.R
@@ -0,0 +1,142 @@
+
+setwd("~/Github/TickMaps")
+
+library(ggthemr)
+library(LaCroixColoR)
+library(tidyverse)
+library(patchwork)
+library(RColorBrewer)
+
+set.seed(5)
+
+tk <- read_csv("TickMaps.csv")
+
+ggthemr('dust')
+x <- ggthemr('flat', set_theme = FALSE)
+y <- ggthemr('light', set_theme = FALSE)
+set.seed(5)
+ramp1 <- colorRampPalette(unlist(x$palette$swatch))(21)
+ramp2 <- colorRampPalette(unlist(y$palette$swatch))(9)
+
+tk %>% select(Date, Pathogen_Disease) %>%
+  mutate(Pathogen_Disease = strsplit(Pathogen_Disease, ",")) %>% 
+  unnest(Pathogen_Disease) %>%
+  group_by(Pathogen_Disease, Date) %>% 
+  tally() %>% 
+  ungroup() %>%
+  na.omit() %>%
+  complete(Pathogen_Disease, Date, fill = list(n = 0)) %>%
+  group_by(Pathogen_Disease) %>% 
+  mutate(Studies = cumsum(n)) %>%
+  dplyr::rename(Disease = Pathogen_Disease) %>%
+  ggplot(aes(x = Date, y = Studies, fill = Disease)) + 
+  geom_area() +
+  theme(legend.position = "bottom",
+        axis.line = element_line(colour = "black"),
+        panel.border = element_rect(colour = 'black', fill = NA),
+        legend.text = element_text(size = 7.5)) +
+  scale_fill_manual(values = ramp1) -> g1
+
+tk %>% select(Date, Vector) %>%
+  mutate(Vector = strsplit(Vector, ",")) %>% 
+  unnest(Vector) %>%
+  mutate(Vector = word(Vector, 1)) %>% 
+  group_by(Vector, Date) %>% 
+  tally() %>% 
+  ungroup() %>%
+  na.omit() %>%
+  complete(Vector, Date, fill = list(n = 0)) %>%
+  group_by(Vector) %>% 
+  mutate(Studies = cumsum(n)) %>%
+  ggplot(aes(x = Date, y = Studies, fill = Vector)) + 
+  geom_area() + 
+  theme(legend.position = "bottom") +
+  theme(axis.line = element_line(colour = "black"),
+        panel.border = element_rect(colour = 'black', fill = NA),
+        legend.text = element_text(size = 7.5)) +
+  scale_fill_manual(values = ramp2) -> g2
+
+g2 / g1
+
+ggthemr_reset()
+
+ggthemr('flat')
+
+tk %>% select(Date, Category) %>%
+  mutate(Category = strsplit(Category, ",")) %>% 
+  unnest(Category) %>%
+  group_by(Category, Date) %>% 
+  tally() %>% 
+  ungroup() %>%
+  na.omit() %>%
+  complete(Category, Date, fill = list(n = 0)) %>%
+  group_by(Category) %>% 
+  mutate(Studies = cumsum(n)) %>%
+  ggplot(aes(x = Date, y = Studies, fill = Category)) + 
+  geom_area() + 
+  theme(legend.position = "bottom") +
+  theme(axis.line = element_line(colour = "black"),
+        panel.border = element_rect(colour = 'black', fill = NA))
+
+################################ 2. Pie Charts
+
+ggthemr('fresh')
+
+rec1 <- c(checked = 'Tick data as proxy', .missing = 'Real pathogen data')
+
+tk %>% mutate(Tick = recode(VectorNoPath, !!!rec1)) %>%
+  count(Tick) %>% 
+  ggplot(aes(x = "", y = n, fill = Tick)) +
+  geom_bar(stat="identity", width=1) +
+  coord_polar("y", start = 0, direction = -1) +
+  theme(legend.position = 'bottom', 
+        legend.title = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        panel.grid  = element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.line = element_blank()) -> g1
+
+rec2 <- c(checked = 'Human case data', .missing = 'No human case data')
+
+tk %>% mutate(Human = recode(`Human case data`, !!!rec2)) %>%
+  count(Human) %>%
+  ggplot(aes(x = "", y = n, fill = Human)) +
+  geom_bar(stat="identity", width=1) +
+  coord_polar("y", start = 0, direction = -1) +
+  theme(legend.position = 'bottom', 
+        legend.title = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        panel.grid  = element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.line = element_blank()) -> g2
+
+
+rec3 <- c(.missing = 'No wildlife data', .default = 'Wildlife data')
+
+tk %>% select(`Host species`) %>% 
+  mutate(Wildlife = recode(`Host species`, !!!rec3)) %>%
+  count(Wildlife) %>%
+  mutate(Wildlife = as_factor(Wildlife)) %>%
+  mutate(Wildlife = fct_relevel(Wildlife, 'Wildlife data', 'No wildlife data')) %>%
+  ggplot(aes(x = "", y = n, fill = Wildlife)) +
+  geom_bar(stat = "identity", width = 1) +
+  coord_polar("y", start = 0, direction = -1) +
+  theme(legend.position = 'bottom', 
+        legend.title = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        panel.grid  = element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.line = element_blank()) -> g3 
+
+(g1 + g2 + g3)
+
+tk %>% select(`Host species`) %>%
+  mutate(Wildlife = strsplit(`Host species`, ",")) %>% 
+  unnest() %>%
+  count(Wildlife)
